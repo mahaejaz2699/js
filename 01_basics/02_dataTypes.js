849d1d12d51c45642f2d73448bf646adf50fd659
Maha Ejaz <mahaejaz26@gmail.com>
2025-06-13 11:23:09 +0100

    Data Types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all JS code as newer version
+
+// alert("hello") //we are using node.js and not browser
+
+console.log(3+3);
+
+console.log("Maha");
+
+let name = "hitesh"
+let age = 26
+let isLoggedIn = false
+let state
+
+//number => 2 to power 53
+//bigint
+//string =>""
+//boolean
+//null
+//undefined
+//symbol
+
+console.log(typeof "Maha")
