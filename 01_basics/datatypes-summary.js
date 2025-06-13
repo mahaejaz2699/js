001548bcb818fbfcd20e1e5f582551f8f018027b
Maha Ejaz <mahaejaz26@gmail.com>
2025-06-13 12:20:11 +0100

    Data Types

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,32 @@
+//Data Types
+
+//Primitive - 7 Types
+
+const score = 100
+const scorValue = 100.3
+const isLoggedIn = false
+const outsideTemp = null
+
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+
+//Non Primitive
+
+const heros = ['Spiderman',"Superman","Batman"]   //Array
+
+let myObj = {        //Object
+    name:"Maha",
+    age:26,
+}
+
+const myfunction = function(){
+    console.log("Hello World");
+}
+
+console.log(typeof id);
+
