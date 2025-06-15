2b69e96ef10cfe4a7c5b4eb1a3cd5d4b3482c00b
Maha Ejaz <mahaejaz26@gmail.com>
2025-06-15 20:25:42 +0100

    Arrays-01

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,35 @@
+const myArr = [0,1,2,3,4,5]
+
+// console.log(myArr[1])
+
+// const myHeroes = ["Spiderman","Batman"]
+
+// const myArr2 = new Array(1,4,5,6)
+// console.log(myArr2[3]);
+
+//Array methods
+
+// myArr.push(6)
+// myArr.push(7)
+// console.log(myArr);
+
+myArr.unshift(0)
+myArr.shift()
+// console.log(myArr);
+
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(3));
+
+const newArr = myArr.join()
+
+// console.log(myArr)
+// console.log(typeof newArr)
+
+//slice,splice
+
+console.log("A",myArr);
+
+const myn1 = myArr.slice(1,3)
+console.log(myn1);
+console.log("B ",myArr)
+
