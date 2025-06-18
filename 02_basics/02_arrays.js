c75066aba90cb1d719a067ccda4deba0e644c78a
Maha Ejaz <mahaejaz26@gmail.com>
2025-06-18 21:43:34 +0100

    Arrays 02

--- 02_basics/02_arrays.js
+++ 02_basics/02_arrays.js
@@ -1,9 +1,32 @@
-const myArr = [0,1,2,3,4,5]
+const marvel_heros = ["thor","ironman","spiderman"]
+const dc = ["superman","flash","batman"]
 
-console.log(myArr);
+// marvel_heros.push(dc)
 
-myArr.push(7)
+// console.log(marvel_heros)
 
-const myArr2 = [0,2,3,4,1,2,3,4,5]
+// console.log(marvel_heros[3][1])
 
-console.log(myArr2)
+const all_heros=marvel_heros.concat(dc)
+console.log(all_heros)
+
+const all_new_heros = [...marvel_heros, ...dc]   //spread
+
+console.log(all_new_heros)
+
+const another_array = [1,2,3,[4,5,6],7,[6,7,[4,5]]]
+
+const real_another_Array = another_array.flat(Infinity)
+console.log(real_another_Array)
+
+console.log(Array.isArray("Maha")) //Asking if it is an array
+
+console.log(Array.from("Maha"))
+
+console.log(Array.from({name:"Maha"}))
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1,score2,score3))
