fef7d45a659f14295f2f80f7b718ba31c05bcd42
Maha Ejaz <mahaejaz26@gmail.com>
2025-06-19 18:32:57 +0100

    Objects

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,58 @@
+// const tinderUser = new Object()
+const tinderUser = {}
+
+tinderUser.id = "123abc"
+tinderUser.name= "Maha"
+tinderUser.isLoggedIn = false
+
+// console.log(tinderUser);
+
+const regularUser = {
+    email: "maha@gmail.com",
+    fullname: {
+        username: {
+            firstname: "maha",
+            lastname: "ejaz"
+        }
+    }
+}
+
+// console.log(regularUser.fullname.username.firstname)
+
+const obj1 = {1: "a", 2:"b"}
+const obj2 = {3: "c", 4:"d"}
+const obj4 = {5: "c", 6:"d"}
+
+// const obj3 = { obj1, obj2 }   //Adding objects
+// const obj3 = Object.assign({},obj1,obj2,obj4)
+
+const obj3 = {...obj1,...obj2,}
+
+// console.log(obj3);
+
+const users = [
+    {
+        id:1,
+        email:"maha@gmail.com"
+    },
+    {
+        id:1,
+        email:"maha@gmail.com"
+    },
+    {
+        id:1,
+        email:"maha@gmail.com"
+    },
+    {
+        id:1,
+        email:"maha@gmail.com"
+    },
+
+]
+
+// console.log(users[1])
+
+// console.log(tinderUser);
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));
