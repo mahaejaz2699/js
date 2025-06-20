b990b0b4ae1f65f170e0e217d9f20c90907f3c99
Maha Ejaz <mahaejaz26@gmail.com>
2025-06-20 23:43:28 +0100

    Functions Add

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -1,4 +1,15 @@
-console.log("M")
+
+function sayMyName(){
+    console.log("M")
 console.log("A")
 console.log("H")
 console.log("A")
+}
+
+// sayMyName()
+
+function addTwoNumbers(number1,number2){
+    console.log(number1+number2);
+}
+
+addTwoNumbers(3,"4")
