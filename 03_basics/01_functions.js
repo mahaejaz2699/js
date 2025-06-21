e8d277556d55c750bc65b137be5b9c209e718900
Maha Ejaz <mahaejaz26@gmail.com>
2025-06-21 10:10:06 +0100

    Functions-01

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -8,10 +8,36 @@ console.log("A")
 
 // sayMyName()
 
+// function addTwoNumbers(number1,number2){
+//     console.log(number1+number2);
+// }
+
+
+
 function addTwoNumbers(number1,number2){
-    console.log(number1+number2);
+    // let result = number1 + number2
+    // console.log("Maha");
+    return number1 + number2
 }
 
 const result = addTwoNumbers(3,5)
 
-console.log(result);
+// console.log(result);
+
+// function loginUserMessage(username){
+//     return `${username} just logged in `
+// }
+
+// console.log(loginUserMessage("Maha"))
+// console.log(loginUserMessage())
+
+
+function loginUserMessage(username = "Maha"){
+    if(!username){    //if username is undefined
+        console.log("Please enter a username");
+        return
+    }
+    return `${username} just logged in`
+}
+
+console.log(loginUserMessage("Haris"))
