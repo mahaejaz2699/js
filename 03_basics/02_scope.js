bf50dea20a7ed2871295ad14fa73b7491d34b005
Maha Ejaz <mahaejaz26@gmail.com>
2025-07-02 19:26:35 +0100

    Scope level

--- 03_basics/02_scope.js
+++ 03_basics/02_scope.js
@@ -5,11 +5,49 @@ let a = 300  //global scope
 if(true){        //block scope
     let a = 10
     const b = 20
-    console.log("Inner",a);
+    // console.log("Inner",a);
 }
 
 
 
-console.log(a);
-// console.log(b);
-// console.log(c);
+// console.log(a);
+// // console.log(b);
+// // console.log(c);
+
+function one(){
+    const username ="Maha"
+
+    function two(){
+        const website = "youtube"
+        console.log(username)
+    }
+
+    // console.log(website);
+
+    // two()
+}
+
+one()
+
+if(true){
+    const username = "Maha"
+    if(username == "Maha"){
+        const website = " youtube"
+        // console.log(username + website);
+    }
+
+    // console.log(website);
+}
+
+// console.log(username);
+
+console.log(addone(5))
+
+function addone(num){
+    return num + 1
+}
+
+// addTwo(5)
+const addTwo = function(num){        
+    return num +2
+}
