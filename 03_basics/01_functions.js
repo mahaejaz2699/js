9371a5ed23b8801460575633bfa0185460c15a06
Maha Ejaz <mahaejaz26@gmail.com>
2025-07-02 19:01:49 +0100

    Functions Arrays

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -40,4 +40,40 @@ function loginUserMessage(username = "Maha"){
     return `${username} just logged in`
 }
 
-console.log(loginUserMessage("Haris"))
+// console.log(loginUserMessage("Haris"))
+
+// console.log(loginUserMessage("Maha"))
+
+// console.log(loginUserMessage("Rameen"))
+
+function calculateCartPrice(val1,val2, ...num1){     //rest operator
+    return num1
+}
+
+// console.log(calculateCartPrice(2))
+
+// console.log(calculateCartPrice(200,300,400,500))
+
+const user = {
+    username: "Maha",
+    price: 199
+}
+
+function handleObject(anyObject){
+    console.log(`Username is ${anyObject.username} and price is ${anyObject.price}`);
+}
+
+handleObject({
+    username: "Maha",
+    price:399
+})
+
+const myNewArray = [200,300,500,600,700]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray));
+
+console.log(returnSecondValue([200,400,100,600]));
