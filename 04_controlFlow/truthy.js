f9ee18222ebb59fc0ba4ee15663449a48efedb43
Maha Ejaz <mahaejaz26@gmail.com>
2025-07-08 22:43:06 +0100

    Null opeator

--- 04_controlFlow/truthy.js
+++ 04_controlFlow/truthy.js
@@ -21,3 +21,17 @@ const emptyObj = {}
 if(Object.keys(emptyObj).length === 0){
     console.log("Object is empty");
 }
+
+// Nullish Coalescing Operator (??): Null defined
+
+let val1;
+// val1 = 5 ?? 10
+// val1 = null ?? 10
+
+// val1 = undefined ?? 15
+
+val1 = null ?? 10 ?? 20
+
+
+
+console.log(val1);
