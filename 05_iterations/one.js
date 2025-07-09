b82704138fc5c5a2053f599c4e14f3d4a8c527ff
Maha Ejaz <mahaejaz26@gmail.com>
2025-07-09 23:59:13 +0100

    Loop

--- 05_iterations/one.js
+++ 05_iterations/one.js
@@ -1,4 +1,5 @@
-for (let index = 0; index < array.length; index++) {
-    const element = array[index];
-    
+for (let index = 0; index < 10; index++) {
+    const element = index;
+    console.log(element);
 }
+
