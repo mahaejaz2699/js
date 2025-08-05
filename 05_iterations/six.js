f8d572dc0775bc7cb4c2d952d4bcfe0f0c8eec41
Maha Ejaz <mahaejaz26@gmail.com>
2025-08-05 12:53:19 +0100

    Filters

--- 05_iterations/six.js
+++ 05_iterations/six.js
@@ -24,3 +24,24 @@ myNums.forEach( (num) =>{
 console.log(newNums);
 
 
+
+
+
+
+const books = [
+    { title : 'Book One', genre: 'Fiction', publish: 1981, edition: 1998 },
+    { title : 'Book Two', genre: 'Non Fiction', publish: 1982, edition: 2008 },
+    { title : 'Book Three', genre: 'History', publish: 1983, edition: 2009 },
+    { title : 'Book Four', genre: 'Science', publish: 1984, edition: 2011 },
+    { title : 'Book Five', genre: 'Fiction', publish: 1985, edition: 2012 },
+    { title : 'Book Six', genre: 'History', publish: 2020, edition: 2016 },
+];
+
+let userBooks = books.filter( (bk) => bk.genre === 'History')
+
+userBooks = books.filter( (bk) => {
+    return bk.publish >= 1985 && bk.genre === "History"
+})
+
+console.log(userBooks);
+
