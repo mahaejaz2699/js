db9f7cc9c6ae4c11e0426933f4acfc4855d0eacf
Maha Ejaz <mahaejaz26@gmail.com>
2025-09-28 14:44:37 +0100

    Project 1

---
+++ chaiaurcode.js
@@ -0,0 +1,8 @@
+const buttons = document.querySelectorAll(`.button`)
+// console.log(buttons);
+
+const body = document.querySelector("body")
+
+buttons.forEach(function(button){
+    console.log(button);
+});
